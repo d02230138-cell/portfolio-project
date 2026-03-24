441261bc77decfeb03dafca5e0ca9d5330e9013e
Your Name <your-email@gmail.com>
2026-03-24 22:02:55 +0530

    fix port issue

--- app.py
+++ app.py
@@ -46,3 +46,8 @@ def contact():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=10000)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
